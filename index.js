a26dba2a012afecebb005968febc1d91a777b749
NICKMUGO <136487345+NICKMUGO@users.noreply.github.com>
2023-08-12 10:32:44 +0300

    C.N. Kihara and advocates

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById('btn').addEventListener("click",(event)=>{
+    event.preventDefault()
+    let pc = document.getElementById("courtsessions")
+    let CourtSession = document.getElementById("description");
+    let br = document.createElement('br');
+    let item1 = document.createElement('li');
+    item1.innerText = CourtSession.value;
+    pc.appendChild(br);
+    pc.appendChild(item1);
+    console.log(item1.innerText);
+  })})
+          
